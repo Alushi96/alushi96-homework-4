b63706c2ddaf7fca78b33652d46f42843c103a72
The Mechanic <jaber4132@gmail.com>
2020-03-04 19:05:48 -0500

    Step 6 - Add Questions

--- script.js
+++ script.js
@@ -67,3 +67,63 @@ hbtn.addEventListener("click", function() {
     init();
     stopTimer();
 })
+
+//Question
+
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var b1 = document.getElementById("choice1");
+var b2 = document.getElementById("choice2");
+var b3 = document.getElementById("choice3");
+var b4 = document.getElementById("choice4");
+
+
+
+let questions = [
+    {
+        question : "Inside which HTML element do we put the JavaScript?",
+        b1 : "javascript",
+        b2 : "scripting",
+        b3 : "script",
+        b4 : "js",
+        correct : "b3",
+    },
+    {
+        question : "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        b1 : "script href='xxx.js'",
+        b2 : "script src='xxx.js'",
+        b3 : "script name='xxx.js'",
+        b4 : "script alt='xxx.js'",
+        correct : "b2",
+    },
+    {
+        question : "How do you write 'Hello World' in an alert box?",
+        b1 : "msg('Hello World');",
+        b2 : "msgBox('Hello World');",
+        b3 : "alertBox('Hello World');",
+        b4 : "alert('Hello World');",
+        correct : "b4",
+    },
+    {
+        question : "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        b1 : "if ( i != 5)",
+        b2 : "if (i <> 5)",
+        b3 : "if i <> 5",
+        b4 : "if i = ! 5 then",
+        correct : "b1",
+    }
+];
+
+var lastquestion = questions.length - 1;
+let runningQuestion = 0;
+
+function renderQuestion() {
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>" + q.question + "</p>";
+    b1.innerHTML = q.b1;
+    b2.innerHTML = q.b2;
+    b3.innerHTML = q.b3;
+    b4.innerHTML = q.b4;
+}
